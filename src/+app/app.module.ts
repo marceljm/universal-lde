195882336cee104bfbabc0a19fba15fbed5a4f53
marceljm <marceljmachado@gmail.com>
2017-02-24 22:32:30 -0300

    partial

--- src/+app/app.module.ts
+++ src/+app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import 'rxjs/add/operator/toPromise';
 
 import { HomeModule } from './+home/home.module';
 import { AboutModule } from './+about/about.module';
@@ -10,18 +13,36 @@ import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent, XLargeDirective } from './app.component';
 
+//import { DataGridModule, PanelModule, ButtonModule, DataTableModule, InputTextModule, GrowlModule } from 'primeng/primeng';
 
 @NgModule({
-  declarations: [ AppComponent, XLargeDirective ],
+  declarations: [
+    AppComponent,
+    XLargeDirective
+  ],
   imports: [
     SharedModule,
     HomeModule,
     AboutModule,
     TodoModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //BrowserModule,
+    //FormsModule,
+    //ReactiveFormsModule,
+    //HttpModule,
+    //JsonpModule,
+    //AppRoutingModule,
+    //DataGridModule,
+    //PanelModule,
+    //ButtonModule,
+    //DataTableModule,
+    //InputTextModule,
+    //GrowlModule
+  ],
+  bootstrap: [
+    //AppComponent
   ]
 })
-export class AppModule {
-}
+export class AppModule { }
 
 export { AppComponent } from './app.component';
